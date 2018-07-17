242f020587a5ccb9e8935c3cd1794891e8ddc285
Zaira Tomayeva <zaira.tomayeva@procore.com>
2018-07-16 22:26:26 -0700

    first commit. initialize android project

---
+++ app/src/main/java/zairat/android/whatsthediff/PullRequests.kt
@@ -0,0 +1,12 @@
+package zairat.android.whatsthediff
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PullRequests : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pull_requests)
+    }
+}
